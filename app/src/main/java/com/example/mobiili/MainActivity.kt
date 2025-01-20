b3d30f47c47c4dc65944e8857b6abc0ee6de398d
AtteB <atte.blomqvist@student.oulu.fi>
2025-01-20 23:39:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mobiili/MainActivity.kt
@@ -0,0 +1,129 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
+import com.example.mobiili.R
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.border
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import com.example.mobiili.SampleData
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.ui.graphics.Color
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTutorialTheme {
+                Conversation(SampleData.conversationSample)
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.john_smith2),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Composable
+fun ComposeTutorialTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = MaterialTheme.colorScheme.copy(
+            primary = Color(0xFF6200EE),
+            surface = Color(0xFFBB86FC)
+        ),
+        typography = MaterialTheme.typography,
+        content = content
+    )
+}
---
+++ app/src/main/java/com/example/mobiili/SampleData.kt
@@ -0,0 +1,89 @@
+package com.example.mobiili
+
+import Message
+
+/**
+ * SampleData for Jetpack Compose Tutorial
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "John Smith",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "John Smith",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "John Smith",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "John Smith",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "John Smith",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "John Smith",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "John Smith",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "John Smith",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "John Smith",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "John Smith",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "John Smith",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "John Smith",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "John Smith",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "John Smith",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "John Smith",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "John Smith",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
